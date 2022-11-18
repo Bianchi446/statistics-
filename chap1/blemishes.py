da5fa83feebb169c3dac7542e73e78eefcbeb8be
Bianchi446 <josebianchi221js@gmail.com>
2022-11-18 10:41:51 -0300

    Frequency distribution of discrete values

--- chap1/blemishes.py
+++ chap1/blemishes.py
@@ -1,13 +1,27 @@
 import mistat
 import pandas as pd
+import matplotlib.pyplot as plt
 
 blemishes = mistat.load_data('BLEMISHES')
 
 #Blemishes is a called a data frame, because its matrix-like structure 
 
-blemishes.head(3)
+blemishes.tail(3)
 
 print(blemishes.iloc[1,0])
 print(blemishes.iloc[2,1])
 
 
+# Use value counts with normalize to get relative frequencies 
+X = pd.DataFrame(blemishes['count'].value_counts(normalize = True))
+X.loc[4, 'count'] = 0
+X = X.sort_index()   # Sort by number of blemishes 
+
+ax = X['count'].plot.bar(color='grey', legend = False)
+ax.set_xlabel('number of blemishes')
+ax.set_ylabel('proportional frequency')
+plt.show()
+
+
+X['Number'] = X.index
+X['cumulative frequency'] = X['count'].cumsum()
